cb63ef3f9d3151d91ad36212529185fe8026623d
Lalith Suresh <lalithadsuresh@users.noreply.github.com>
2025-02-27 20:25:00 -0500

    set up mongoDB + express

---
+++ frontend/my-react-project/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './Homepage';
+import Matches from './Matches';
+import Settings from './Settings';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Homepage />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
