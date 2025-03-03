2c49029ddb23627bbfbb5ab49961ff05c536482d
Shlok kaul <Shlokkaul@Macbook-13.local>
2025-03-03 17:18:56 -0500

    added some buttons and pages

--- frontend/my-react-project/src/App.js
+++ frontend/my-react-project/src/App.js
@@ -1,19 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Homepage from './Homepage';
 import Matches from './Matches';
 import Settings from './Settings';
+import Navbar from './Navbar';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Homepage />
+    // <div className="App">
+    //   <header className="App-header">
+
+    //     <Navbar />
         
-      </header>
-    </div>
+    //   </header>
+    // </div>
+    
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path = '/' exact />
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/matches" element={<Matches />} />
+        {/* <Route path="/profile" element={<Profile />} /> */}
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
+    </>
+
   );
 }
 
