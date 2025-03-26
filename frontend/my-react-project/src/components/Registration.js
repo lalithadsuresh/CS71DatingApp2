528fe92b7504b83f1863928b63927e8f65a64cef
Shlok kaul <Shlokkaul@Macbook-13.local>
2025-03-25 20:01:39 -0400

    added form to registration and database

--- frontend/my-react-project/src/components/Registration.js
+++ frontend/my-react-project/src/components/Registration.js
@@ -13,8 +13,17 @@ const Registration = () => {
     const [formData, setFormData] = useState({
 
         name: '',
-        age: ''
-
+        age: '',
+        location: '',
+        pronouns: '',
+        genderIdentity: '',
+        datePreference: '',
+        relationshipType: '',
+        ethnicity: '',
+        religion: '',
+        bio: '',
+        education: '',
+        job: ''
     });
 
     const handleChange = (e) => {
@@ -56,8 +65,7 @@ const Registration = () => {
     }
 
     return (
-
-        <form onSubmit={handleSubmit}> 
+/*         <form onSubmit={handleSubmit}> 
             <input
                 type="text"
                 name="name"
@@ -76,11 +84,177 @@ const Registration = () => {
                 Register
             </button>
         </form>
+*/
 
 
+
+
+        <div className="container py-5">
+            <form onSubmit={handleSubmit}> 
+
+                <h2 className="mb-5">Personal Information</h2>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        className="form-control"
+                        id="name"
+                        onChange={handleChange}>
+                    </input>
+                </div>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="age">Age</label>
+                    <input
+                        type="number"
+                        name="age"
+                        value={formData.age}
+                        className="form-control"
+                        id="age"
+                        onChange={handleChange}>
+                    </input>
+                </div>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="location">Location</label>
+                    <input
+                        type="text"
+                        name="location"
+                        value={formData.location}
+                        className="form-control"
+                        id="location"
+                        onChange={handleChange}>
+                    </input>
+                </div>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="pronouns">Pronouns</label>
+                    <input
+                        type="text"
+                        name="pronouns"
+                        value={formData.pronouns}
+                        className="form-control"
+                        id="pronouns"
+                        onChange={handleChange}>
+                    </input>
+                </div>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="genderIdentity">Gender Identity</label>
+                    <input
+                        type="text"
+                        name="genderIdentity"
+                        value={formData.genderIdentity}
+                        className="form-control"
+                        id="genderIdentity"
+                        onChange={handleChange}>
+                    </input>
+                </div>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="datePreference">Who do you want to date? (Gender)</label>
+                    <input
+                        type="text"
+                        name="datePreference"
+                        value={formData.datePreference}
+                        className="form-control"
+                        id="datePreference"
+                        onChange={handleChange}>
+                    </input>
+                </div>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="relationshipType">Relationship Type</label>
+                    <select
+                        name="relationshipType"
+                        value={formData.relationshipType}
+                        className="form-control"
+                        id="relationshipType"
+                        onChange={handleChange}>
+                        <option value="">Select an option</option>
+                        <option>Life Partner</option>
+                        <option>Long-term Relationship</option>
+                        <option>Short-term Relationship</option>
+                        <option>Unsure</option>
+                        <option>Prefer not to say</option>
+                    </select>
+                </div>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="ethnicity">Ethnicity</label>
+                    <input
+                        type="text"
+                        name="ethnicity"
+                        value={formData.ethnicity}
+                        className="form-control"
+                        id="ethnicity"
+                        onChange={handleChange}>
+                    </input>
+                </div>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="religion">Religion</label>
+                    <input
+                        type="text"
+                        name="religion"
+                        value={formData.religion}
+                        className="form-control"
+                        id="religion"
+                        onChange={handleChange}>
+                    </input>
+                </div>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="bio">Short Bio</label>
+                    <textarea
+                        name="bio"
+                        value={formData.bio}
+                        className="form-control"
+                        id="bio"
+                        rows="4"
+                        onChange={handleChange}>
+                    </textarea>
+                </div>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="education">Education</label>
+                    <input
+                        type="text"
+                        name="education"
+                        value={formData.education}
+                        className="form-control"
+                        id="education"
+                        onChange={handleChange}>
+                    </input>
+                </div>
+
+                <div className="form-group mb-5">
+                    <label htmlFor="job">Job</label>
+                    <input
+                        type="text"
+                        name="job"
+                        value={formData.job}
+                        className="form-control"
+                        id="job"
+                        onChange={handleChange}>
+                    </input>
+                </div>
+
+                <button type="submit" className="btn btn-primary mt-4"> 
+                    Submit
+                </button>
+
+            </form>
+        </div>
+
+        
+
+    
     );
-
-
+    
 
 };
 
