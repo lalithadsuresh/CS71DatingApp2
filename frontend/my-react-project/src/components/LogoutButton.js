7ff76cf099598572e8ab85b0804976ab28ff2a28
Ray James <rayjames@dhcp-130-58-169-11.swarthmore.edu>
2025-04-10 00:12:00 -0400

    logout button on navbar

--- frontend/my-react-project/src/components/LogoutButton.js
+++ frontend/my-react-project/src/components/LogoutButton.js
@@ -1,10 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 
-const LogoutButton = () => {
+const LogoutButton = ({visible = false}) => {
 
     const { logout, isAuthenticated } = useAuth0();
     console.log("Is Authenticated:", isAuthenticated);
+    if (!isAuthenticated || !visible) return null;
 
     return (
         isAuthenticated && (
