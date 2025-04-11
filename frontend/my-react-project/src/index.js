57ca918bf2eb7761a23df336bba8a7d02ab14345
Lalith Suresh <lalithadsuresh@users.noreply.github.com>
2025-04-11 14:52:19 -0400

    removed StrictMode

--- frontend/my-react-project/src/index.js
+++ frontend/my-react-project/src/index.js
@@ -10,7 +10,6 @@ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Auth0Provider
       domain = {domain}
       clientId = {clientId}
@@ -20,7 +19,6 @@ root.render(
       cacheLocation="localstorage">     
       <App />
     </Auth0Provider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
